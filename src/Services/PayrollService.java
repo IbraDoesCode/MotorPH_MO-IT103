a960dbf5bb7da1b0e6cda8f4d3de29556ab4be14
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2024-06-07 18:44:37 +0800

    implemented payroll service for payroll calculation

--- src/Services/PayrollService.java
+++ src/Services/PayrollService.java
@@ -1,4 +1,13 @@
 package Services;
 
 public class PayrollService {
+
+    public static double getSemiMonthlyRate(double BasicSalary) {
+        return BasicSalary / 2;
+    }
+
+    public static double getHourlyRate(double BasicSalary) {
+        return (BasicSalary / 21) / 8;
+    }
+
 }
