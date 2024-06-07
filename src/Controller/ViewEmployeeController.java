8e74951f8af2d758246ed065abd1b9f5007d174e
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2024-06-07 18:47:00 +0800

    implemented update record functionality

--- src/Controller/ViewEmployeeController.java
+++ src/Controller/ViewEmployeeController.java
@@ -1,7 +1,9 @@
 package Controller;
 
+import Model.DataHandler;
 import Model.Employee;
 import Model.EmployeeDataHandler;
+import Services.PayrollService;
 import com.opencsv.exceptions.CsvException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,6 +14,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import org.w3c.dom.Text;
 import ui.AlertUtil;
 
 import java.io.IOException;
@@ -47,6 +50,12 @@ public class ViewEmployeeController {
     @FXML
     private TextField basicSalary_TextField;
     @FXML
+    private TextField riceSubsidy_TextField;
+    @FXML
+    private TextField clothingAllowance_TextField;
+    @FXML
+    private TextField phoneAllowance_TextField;
+    @FXML
     private StackPane contentArea;
 
     public void setEmployeeDetails(Employee employee) {
@@ -64,6 +73,9 @@ public class ViewEmployeeController {
         tin_TextField.setText(employee.getTinNumber());
         pagIbig_TextField.setText(employee.getPagibigNumber());
         basicSalary_TextField.setText(String.valueOf(employee.getBasicSalary()));
+        riceSubsidy_TextField.setText(String.valueOf(employee.getRiceSubsidy()));
+        clothingAllowance_TextField.setText(String.valueOf(employee.getClothingAllowance()));
+        phoneAllowance_TextField.setText(String.valueOf(employee.getPhoneAllowance()));
     }
 
     @FXML
@@ -93,4 +105,65 @@ public class ViewEmployeeController {
         }
 
     }
+
+    @FXML
+    private void updateEmployeeRecord() {
+        String[] fullName = fullname_TextField.getText().split(" ");
+
+        String employee_id = employeeId_TextField.getText();
+        String lastname = fullName.length > 1 ? fullName[1] : "";
+        String firstname = fullName.length > 0 ? fullName[0] : "";
+        String dob = dob_TextField.getText();
+        String address = address_TextArea.getText();
+        String phoneNo = contactNo_TextField.getText();
+        String sss = sss_TextField.getText();
+        String philhealth = philhealth_TextField.getText();
+        String tin = tin_TextField.getText();
+        String pagibig = pagIbig_TextField.getText();
+        String status = status_TextField.getText();
+        String position = position_TextField.getText();
+        String department = department_TextField.getText();
+        String supervisor = supervisor_TextField.getText();
+        String basicSalary = basicSalary_TextField.getText();
+        String riceSubsidy = riceSubsidy_TextField.getText();
+        String clothingAllowance = clothingAllowance_TextField.getText();
+        String phoneAllowance = phoneAllowance_TextField.getText();
+        String semiMonthly = "";
+        String hourlyRate = "";
+
+
+        if (employee_id.isEmpty() || lastname.isEmpty() || firstname.isEmpty() || dob.isEmpty() || address.isEmpty() ||
+                phoneNo.isEmpty() || sss.isEmpty() || philhealth.isEmpty() || tin.isEmpty() || pagibig.isEmpty() ||
+                status.isEmpty() || position.isEmpty() || department.isEmpty() || supervisor.isEmpty() || basicSalary.isEmpty() ||
+                riceSubsidy.isEmpty() || clothingAllowance.isEmpty() || phoneAllowance.isEmpty()) {
+            AlertUtil.showAlert(Alert.AlertType.ERROR, "Error", "All fields are required");
+            return;
+        }
+
+
+        try {
+            semiMonthly = String.valueOf(PayrollService.getSemiMonthlyRate(Double.parseDouble(basicSalary)));
+            hourlyRate = String.valueOf(PayrollService.getHourlyRate(Double.parseDouble(basicSalary)));
+        } catch (NumberFormatException e) {
+            AlertUtil.showAlert(Alert.AlertType.ERROR, "Error", "Invalid numeric input: " + e.getMessage());
+            return;
+        }
+
+        String[] employeeData = {employee_id, lastname, firstname, dob, address, phoneNo, sss, philhealth, tin, pagibig,
+                status, position, department, supervisor, basicSalary, riceSubsidy, clothingAllowance, phoneAllowance,
+                semiMonthly, hourlyRate};
+
+        boolean confirmed = AlertUtil.showConfirmationAlert("Warning", "Update Employee Record?");
+
+        if (confirmed) {
+            int id = Integer.parseInt(employee_id);
+            try {
+                DataHandler.updateEmployeeRecord(id, employeeData);
+                AlertUtil.showAlert(Alert.AlertType.INFORMATION, "Success", "Employee record updated");
+            } catch (IOException | CsvException e) {
+                AlertUtil.showAlert(Alert.AlertType.ERROR, "Error", e.getMessage());
+            }
+        }
+    }
+
 }
